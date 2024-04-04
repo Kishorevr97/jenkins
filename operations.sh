7763ca776b4e8735952a3bff82b4b05321ba2231
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2024-04-04 20:52:21 +0530

    Create operations.sh

---
+++ operations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sum=`expr 10 + 20`
+product=`expr 2 \* 8`
+div=`expr 8 / 2`
+echo "the sum of two numbers is $sum"
+echo "product of two nmbers is $product"
+echo "division of two numbers is $div"
